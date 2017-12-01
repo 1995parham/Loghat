ed189eb11049f5f26e1bf6c45ada9606f5027ea3
Parham Alvani <parham.alvani@gmail.com>
2017-12-01 21:51:33 +0330

    Fetches and Decode :muscle:.

--- mw/mw.go
+++ mw/mw.go
@@ -11,9 +11,23 @@
 // Package mw is merriam webster api client package
 package mw
 
+import (
+	"encoding/xml"
+	"fmt"
+	"net/http"
+)
+
 // MerriamWebster type contains word defination
 type MerriamWebster struct {
-	Word string
+	Word    string
+	Version string
+}
+
+// Word represents word definition with some description
+type Word struct {
+	Date   string
+	Def    string
+	Extera string
 }
 
 // New creates merriam-webster object contains given word definition
@@ -25,5 +39,25 @@ func New(word string) *MerriamWebster {
 
 // Fetch word definition
 func (m *MerriamWebster) Fetch() error {
+	resp, err := http.Get(fmt.Sprintf("https://www.dictionaryapi.com/api/v1/references/collegiate/xml/%s?key=8159d5d5-7a05-4e4d-a1d5-001b81ceb618", m.Word))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Status error: %v", resp.StatusCode)
+	}
+
+	var el EntryList
+	if err = xml.NewDecoder(resp.Body).Decode(&el); err != nil {
+		return err
+	}
+
+	m.Version = el.Version
+	for _, e := range el.Entries {
+		fmt.Println(e.Def)
+	}
+
 	return nil
 }
