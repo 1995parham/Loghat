cf6025b67582ffd69ab4b24cbb9de8f09237e176
Parham Alvani <parham.alvani@gmail.com>
2017-12-01 23:15:30 +0330

    First create merriam webster client then fetch words.

--- main.go
+++ main.go
@@ -19,17 +19,17 @@ import (
 
 func main() {
 	flag.Parse()
+	m := mw.New("8159d5d5-7a05-4e4d-a1d5-001b81ceb618", "collegiate")
 
 	for _, w := range flag.Args() {
-		m := mw.New(w)
 
-		err := m.Fetch()
+		w, err := m.Fetch(w)
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 
-		for _, d := range m.Definitions {
+		for _, d := range w.Definitions {
 			fmt.Println(d)
 		}
 	}
