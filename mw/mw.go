071522d51cbee615e22c99717914e9ea92853a8e
Parham Alvani <parham.alvani@gmail.com>
2017-12-01 23:15:06 +0330

    Create merriam webster object for key and target dictionary.

--- mw/mw.go
+++ mw/mw.go
@@ -17,8 +17,14 @@ import (
 	"net/http"
 )
 
-// MerriamWebster type contains word defination
+// MerriamWebster contains information for create connection into merriam-webster
 type MerriamWebster struct {
+	key        string
+	dictionary string
+}
+
+// Word type contains word defination
+type Word struct {
 	Word        string
 	Version     string
 	Definitions []Definition
@@ -32,38 +38,40 @@ type Definition struct {
 	Extera string
 }
 
-// New creates merriam-webster object contains given word definition
-func New(word string) *MerriamWebster {
+// New creates merriam-webster object with given key and dictionary
+func New(key string, dictionary string) *MerriamWebster {
 	return &MerriamWebster{
-		Word: word,
+		key:        key,
+		dictionary: dictionary,
 	}
 }
 
 // Fetch word definition
-func (m *MerriamWebster) Fetch() error {
-	resp, err := http.Get(fmt.Sprintf("https://www.dictionaryapi.com/api/v1/references/collegiate/xml/%s?key=8159d5d5-7a05-4e4d-a1d5-001b81ceb618", m.Word))
+func (m *MerriamWebster) Fetch(word string) (*Word, error) {
+	resp, err := http.Get(fmt.Sprintf("https://www.dictionaryapi.com/api/v1/references/%s/xml/%s?key=%s", m.dictionary, word, m.key))
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("Status error: %v", resp.StatusCode)
+		return nil, fmt.Errorf("Status error: %v", resp.StatusCode)
 	}
 
 	var el EntryList
 	if err = xml.NewDecoder(resp.Body).Decode(&el); err != nil {
-		return err
+		return nil, err
 	}
 
-	m.Version = el.Version
+	var w Word
+	w.Version = el.Version
 	for _, e := range el.Entries {
-		m.Definitions = append(m.Definitions, Definition{
+		w.Definitions = append(w.Definitions, Definition{
 			Date: e.Date,
 			Def:  e.Def,
 			Type: e.Type,
 		})
 	}
 
-	return nil
+	return &w, nil
 }
